0cad82e84a41cd270544619481748e92583bf0f7
saeed <test@gmail.com>
2024-05-16 20:52:07 +0330

    Add StringToPersianDigits extension

--- core/utils/lib/extension/strings.dart
+++ core/utils/lib/extension/strings.dart
@@ -1,3 +1,17 @@
 extension StringNullity on String? {
   bool get isNullOrEmpty => this?.isEmpty ?? true;
 }
+
+extension StringToPersianDigits on String {
+  String get toPersianDigits => replaceEnglishWithPersianNumbers(this);
+}
+
+String replaceEnglishWithPersianNumbers(String input) {
+  const englishNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const persianNumbers = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+
+  for (int i = 0; i < englishNumbers.length; i++) {
+    input = input.replaceAll(englishNumbers[i], persianNumbers[i]);
+  }
+  return input;
+}
