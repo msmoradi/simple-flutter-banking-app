8122cab9304da14f72ab9c22c6b11def24c8d2d2
saeed <test@gmail.com>
2024-05-22 16:30:44 +0330

    Chane NationalId validator

--- core/utils/lib/extension/national_id.dart
+++ core/utils/lib/extension/national_id.dart
@@ -2,7 +2,6 @@ import 'package:domain/entities/national_id.dart';
 
 extension NationalIdExtension on NationalId {
   bool isValid() {
-    return RegExp(r'^(?:0|\+98|0098)?9\d{9}$|^(?:0|\+98|0098)21\d{8}$')
-        .hasMatch(value);
+    return value.length == 10;
   }
 }
