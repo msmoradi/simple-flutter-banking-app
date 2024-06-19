f92044d43ba8143a450fc9a36ae53b1eda4f809f
saeed <test@gmail.com>
2024-06-19 17:38:32 +0330

    Fix phone form theme style

--- feature/phone/lib/presentation/view/phone_form.dart
+++ feature/phone/lib/presentation/view/phone_form.dart
@@ -1,5 +1,6 @@
 import 'package:designsystem/widgets/button/fill/full_fill_button.dart';
 import 'package:designsystem/widgets/textfields/phone_number_text_field.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:utils/extension/build_context.dart';
@@ -35,7 +36,7 @@ class _PhoneFormState extends State<PhoneForm> {
           child: Text(
             style: Theme.of(context)
                 .textTheme
-                .headlineMedium
+                .headlineLarge
                 ?.copyWith(fontWeight: FontWeight.bold),
             translator.mobilePhoneNumber,
           ),
@@ -56,13 +57,48 @@ class _PhoneFormState extends State<PhoneForm> {
             )),
         const Spacer(),
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-          child: Text(
-            "با ثبت نام در بنکس، با شرایط استفاده و سیاست حفظ حریم خصوصی ما موافقت می‌کنید",
-            style: Theme.of(context).textTheme.bodyMedium,
-            textAlign: TextAlign.center,
-          ),
-        ),
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                  text: 'با ثبت نام در بنکس، با ',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodySmall
+                      ?.copyWith(color: Theme.of(context).colorScheme.primary),
+                  children: [
+                    TextSpan(
+                      text: 'شرایط استفاده',
+                      style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                          color: Theme.of(context).colorScheme.secondary),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          // Handle click event here
+                          print('Gold text clicked!');
+                        },
+                    ),
+                    TextSpan(
+                      text: ' و ',
+                      style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                          color: Theme.of(context).colorScheme.primary),
+                    ),
+                    TextSpan(
+                      text: 'سیاست حفظ حریم خصوصی',
+                      style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                          color: Theme.of(context).colorScheme.secondary),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          // Handle click event here
+                          print('Gold text clicked!');
+                        },
+                    ),
+                    TextSpan(
+                      text: ' ما موافقت می‌کنید',
+                      style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                          color: Theme.of(context).colorScheme.primary),
+                    )
+                  ]),
+            )),
         const SizedBox(height: 24),
         PrimaryFillButton(
           onPressed: () {
