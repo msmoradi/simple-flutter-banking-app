d8a34f98306804675589a64e177dac6168a8f306
saeed <test@gmail.com>
2024-06-19 17:39:18 +0330

    Add text themes

--- core/designsystem/lib/theme/text_styles.dart
+++ core/designsystem/lib/theme/text_styles.dart
@@ -1,4 +1,98 @@
+import 'package:flutter/material.dart';
 
 class TextStyles {
   static const fontFamilyName = 'IRANSansXFaNum';
+
+  static TextTheme falomenTextTheme = const TextTheme(
+    displayLarge: TextStyle(
+      fontFamily: fontFamilyName,
+      fontSize: 57,
+      height: 64,
+      fontWeight: FontWeight.bold,
+    ),
+    displayMedium: TextStyle(
+      fontFamily: fontFamilyName,
+      fontSize: 45,
+      height: 52,
+      fontWeight: FontWeight.bold,
+    ),
+    displaySmall: TextStyle(
+      fontFamily: fontFamilyName,
+      fontSize: 36,
+      height: 44,
+      fontWeight: FontWeight.normal,
+    ),
+    headlineLarge: TextStyle(
+      fontFamily: fontFamilyName,
+      fontSize: 32,
+      height: 38.4,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineMedium: TextStyle(
+      fontFamily: fontFamilyName,
+      fontSize: 24,
+      height: 28.8,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineSmall: TextStyle(
+      fontFamily: fontFamilyName,
+      fontSize: 18,
+      height: 21.6,
+      fontWeight: FontWeight.bold,
+    ),
+    titleLarge: TextStyle(
+      fontFamily: fontFamilyName,
+      fontSize: 22,
+      height: 28,
+      fontWeight: FontWeight.normal,
+    ),
+    titleMedium: TextStyle(
+      fontFamily: fontFamilyName,
+      fontSize: 16,
+      height: 24,
+      fontWeight: FontWeight.normal,
+    ),
+    titleSmall: TextStyle(
+      fontFamily: fontFamilyName,
+      fontSize: 14,
+      height: 20,
+      fontWeight: FontWeight.normal,
+    ),
+    bodyLarge: TextStyle(
+      fontFamily: fontFamilyName,
+      fontSize: 16,
+      height: 24,
+      fontWeight: FontWeight.normal,
+    ),
+    bodyMedium: TextStyle(
+      fontFamily: fontFamilyName,
+      fontSize: 14,
+      height: 20,
+      fontWeight: FontWeight.normal,
+    ),
+    bodySmall: TextStyle(
+      fontFamily: fontFamilyName,
+      fontSize: 12,
+      height: 16,
+      fontWeight: FontWeight.normal,
+    ),
+    labelLarge: TextStyle(
+      fontFamily: fontFamilyName,
+      fontSize: 14,
+      height: 20,
+      fontWeight: FontWeight.w600,
+    ),
+    labelMedium: TextStyle(
+      fontFamily: fontFamilyName,
+      fontSize: 12,
+      height: 16,
+      fontWeight: FontWeight.bold,
+    ),
+    labelSmall: TextStyle(
+      fontFamily: fontFamilyName,
+      fontSize: 12,
+      height: 16,
+      fontWeight: FontWeight.w500,
+    ),
+  );
 }
