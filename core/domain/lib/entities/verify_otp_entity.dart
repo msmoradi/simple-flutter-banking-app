0f34590d00f86a4e1e16e594ef11d980e9658192
saeed <test@gmail.com>
2024-06-20 21:48:10 +0330

    Add VerifyOtpEntity

--- core/domain/lib/entities/verify_otp_entity.dart
+++ core/domain/lib/entities/verify_otp_entity.dart
@@ -1,5 +1,14 @@
 import 'package:domain/entities/entity.dart';
+import 'package:domain/entities/password_authentication.dart';
 
 class VerifyOtpEntity extends Entity {
-  VerifyOtpEntity();
+  final String accessToken;
+  final String refreshToken;
+  final PasswordAuthentication passwordAuthentication;
+
+  VerifyOtpEntity({
+    required this.accessToken,
+    required this.refreshToken,
+    required this.passwordAuthentication,
+  });
 }
