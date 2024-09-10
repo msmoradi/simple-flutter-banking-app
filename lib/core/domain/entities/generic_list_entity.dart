67a2d09347f546e5f8367f7224962f03598a1b71
msmoradi <mr.ms.moradi@gmail.com>
2024-09-11 02:12:54 +0330

    Clear generic_list_entity

--- lib/core/domain/entities/generic_list_entity.dart
+++ lib/core/domain/entities/generic_list_entity.dart
@@ -1,38 +1,63 @@
 import 'package:banx/core/domain/entities/entity.dart';
 
 class GenericListEntity<T> extends Entity {
+  final int totalPages;
+  final int totalElements;
+  final PageableEntity pageable;
+  final bool first;
+  final bool last;
+  final int numberOfElements;
+  final int size;
+  final List<T> content;
+  final int number;
+  final List<SortEntity> sort;
+  final bool empty;
+
   GenericListEntity({
-    required int totalPages,
-    required int totalElements,
-    required PageableEntity pageable,
-    required bool first,
-    required bool last,
-    required int numberOfElements,
-    required int size,
-    required List<T> content,
-    required int number,
-    required List<SortEntity> sort,
-    required bool empty,
+    required this.totalPages,
+    required this.totalElements,
+    required this.pageable,
+    required this.first,
+    required this.last,
+    required this.numberOfElements,
+    required this.size,
+    required this.content,
+    required this.number,
+    required this.sort,
+    required this.empty,
   });
 }
 
 class PageableEntity extends Entity {
+  final bool unpaged;
+  final int pageSize;
+  final bool paged;
+  final int pageNumber;
+  final int offset;
+  final List<SortEntity> sort;
+
   PageableEntity({
-    required bool unpaged,
-    required int pageSize,
-    required bool paged,
-    required int pageNumber,
-    required int offset,
-    required List<SortEntity> sort,
+    required this.unpaged,
+    required this.pageSize,
+    required this.paged,
+    required this.pageNumber,
+    required this.offset,
+    required this.sort,
   });
 }
 
 class SortEntity extends Entity {
+  final String direction;
+  final String nullHandling;
+  final bool ascending;
+  final String property;
+  final bool ignoreCase;
+
   SortEntity({
-    required String direction,
-    required String nullHandling,
-    required bool ascending,
-    required String property,
-    required bool ignoreCase,
+    required this.direction,
+    required this.nullHandling,
+    required this.ascending,
+    required this.property,
+    required this.ignoreCase,
   });
 }
