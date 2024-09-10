eee55689dd6a60faca1f707196f55314390a7af8
msmoradi <mr.ms.moradi@gmail.com>
2024-09-11 02:13:48 +0330

    Replace loading container

--- lib/feature/kyc_status/presentation/view/kyc_status_content.dart
+++ lib/feature/kyc_status/presentation/view/kyc_status_content.dart
@@ -1,10 +1,10 @@
 import 'package:banx/core/designsystem/widgets/button/fill/full_fill_button.dart';
+import 'package:banx/core/designsystem/widgets/components/loading_container.dart';
 import 'package:banx/core/designsystem/widgets/info_text_row.dart';
 import 'package:banx/core/domain/entities/kyc_status_dto.dart';
 import 'package:banx/feature/kyc_status/mapper/kyc_state_mapper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class KycStatusContent extends StatelessWidget {
   final Function() onActionClick;
@@ -45,108 +45,97 @@ class KycStatusContent extends StatelessWidget {
         ),
       ),
       body: SafeArea(
-        child: AnimatedSwitcher(
-          duration: const Duration(milliseconds: 200),
-          transitionBuilder: (child, animation) =>
-              ScaleTransition(scale: animation, child: child),
-          child: showLoading
-              ? Center(
-                  child: LoadingAnimationWidget.threeRotatingDots(
-                    color: Theme.of(context).colorScheme.onSurfaceVariant,
-                    size: 70.0,
-                  ),
-                )
-              : Column(
-                  children: [
-                    Expanded(
-                      child: SingleChildScrollView(
+        child: LoadingContainer(
+          showLoading: showLoading,
+          content: Column(
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
                         child: Column(
                           children: [
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 16.0),
-                              child: Column(
-                                children: [
-                                  Align(
-                                    alignment: Alignment.centerRight,
-                                    child: Text(
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .bodyMedium
-                                          ?.copyWith(
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .onSurfaceVariant,
-                                          ),
-                                      "استعلام از بانک مرکزی جهت افتتاح حساب در حال انجام است",
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium
+                                    ?.copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onSurfaceVariant,
                                     ),
-                                  ),
-                                  const SizedBox(height: 40),
-                                  InfoTextRowWidget(
-                                    iconAsset: identity!.status.toIconAssets(),
-                                    title: identity!.title,
-                                    subtitle: identity!.description,
-                                    background: identity!.status
-                                        .toBackgroundColor(context),
-                                  ),
-                                  const SizedBox(height: 30),
-                                  InfoTextRowWidget(
-                                    iconAsset:
-                                        phoneNumber!.status.toIconAssets(),
-                                    title: phoneNumber!.title,
-                                    subtitle: phoneNumber!.description,
-                                    background: phoneNumber!.status
-                                        .toBackgroundColor(context),
-                                  ),
-                                  const SizedBox(height: 30),
-                                  InfoTextRowWidget(
-                                    iconAsset: face!.status.toIconAssets(),
-                                    title: face!.title,
-                                    subtitle: face!.description,
-                                    background:
-                                        face!.status.toBackgroundColor(context),
-                                  ),
-                                  const SizedBox(height: 30),
-                                  InfoTextRowWidget(
-                                    iconAsset: sayah!.status.toIconAssets(),
-                                    title: sayah!.title,
-                                    subtitle: sayah!.description,
-                                    background: sayah!.status
-                                        .toBackgroundColor(context),
-                                  ),
-                                ],
+                                "استعلام از بانک مرکزی جهت افتتاح حساب در حال انجام است",
                               ),
                             ),
-                            const SizedBox(height: 64),
-                            Image.asset(
-                              "assets/images/kyc_status.png",
-                              fit: BoxFit.fitWidth,
-                              width: double.infinity,
-                              alignment: Alignment.center,
+                            const SizedBox(height: 40),
+                            InfoTextRowWidget(
+                              iconAsset: identity!.status.toIconAssets(),
+                              title: identity!.title,
+                              subtitle: identity!.description,
+                              background:
+                                  identity!.status.toBackgroundColor(context),
+                            ),
+                            const SizedBox(height: 30),
+                            InfoTextRowWidget(
+                              iconAsset: phoneNumber!.status.toIconAssets(),
+                              title: phoneNumber!.title,
+                              subtitle: phoneNumber!.description,
+                              background: phoneNumber!.status
+                                  .toBackgroundColor(context),
+                            ),
+                            const SizedBox(height: 30),
+                            InfoTextRowWidget(
+                              iconAsset: face!.status.toIconAssets(),
+                              title: face!.title,
+                              subtitle: face!.description,
+                              background:
+                                  face!.status.toBackgroundColor(context),
+                            ),
+                            const SizedBox(height: 30),
+                            InfoTextRowWidget(
+                              iconAsset: sayah!.status.toIconAssets(),
+                              title: sayah!.title,
+                              subtitle: sayah!.description,
+                              background:
+                                  sayah!.status.toBackgroundColor(context),
                             ),
                           ],
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: PrimaryFillButton(
-                        label: actionTitle,
-                        icon: SvgPicture.asset(
-                          "assets/icons/$actionIcon.svg",
-                          colorFilter: ColorFilter.mode(
-                            Theme.of(context).colorScheme.onPrimary,
-                            BlendMode.srcIn,
-                          ),
-                          width: 24,
-                          height: 24,
-                        ),
-                        onPressed: onActionClick,
+                      const SizedBox(height: 64),
+                      Image.asset(
+                        "assets/images/kyc_status.png",
+                        fit: BoxFit.fitWidth,
+                        width: double.infinity,
+                        alignment: Alignment.center,
                       ),
-                    ),
-                    const SizedBox(height: 16),
-                  ],
+                    ],
+                  ),
                 ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: PrimaryFillButton(
+                  label: actionTitle,
+                  icon: SvgPicture.asset(
+                    "assets/icons/$actionIcon.svg",
+                    colorFilter: ColorFilter.mode(
+                      Theme.of(context).colorScheme.onPrimary,
+                      BlendMode.srcIn,
+                    ),
+                    width: 24,
+                    height: 24,
+                  ),
+                  onPressed: onActionClick,
+                ),
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
         ),
       ),
     );
