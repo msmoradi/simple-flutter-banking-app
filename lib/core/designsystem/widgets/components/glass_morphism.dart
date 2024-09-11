48619a8908a908fd2ffa2d41c7dc6c4ea14cb1ea
msmoradi <mr.ms.moradi@gmail.com>
2024-09-11 23:22:41 +0330

    Add gradient opacity effect in GlassMorphism widget

--- lib/core/designsystem/widgets/components/glass_morphism.dart
+++ lib/core/designsystem/widgets/components/glass_morphism.dart
@@ -11,6 +11,7 @@ class GlassMorphism extends StatelessWidget {
       required this.color,
       this.borderRadius})
       : super(key: key);
+
   final Widget child;
   final double blur;
   final double opacity;
@@ -20,11 +21,21 @@ class GlassMorphism extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
+      borderRadius: borderRadius ?? BorderRadius.zero,
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaX: blur, sigmaY: blur),
         child: Container(
           decoration: BoxDecoration(
-              color: color.withOpacity(opacity), borderRadius: borderRadius),
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                color.withOpacity(opacity), // Top is blurred
+                Theme.of(context).colorScheme.surface // Bottom is clear
+              ],
+            ),
+            borderRadius: borderRadius,
+          ),
           child: child,
         ),
       ),
