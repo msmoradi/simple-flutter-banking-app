ae3dc3a1e64a3c6aa7e29d0ff67c8d7ee44e6bea
msmoradi <mr.ms.moradi@gmail.com>
2024-09-14 05:36:53 +0330

    Add error handling

--- lib/core/networking/interceptors/response_interceptor.dart
+++ lib/core/networking/interceptors/response_interceptor.dart
@@ -4,11 +4,12 @@ import 'package:injectable/injectable.dart';
 
 @injectable
 class ResponseInterceptor extends Interceptor {
-
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
     if (response.statusCode == 403) {
       _handle403Error();
+    } else if (response.statusCode == 401) {
+      _handle401Error();
     } else {
       super.onResponse(response, handler);
     }
@@ -18,12 +19,19 @@ class ResponseInterceptor extends Interceptor {
   void onError(DioException err, ErrorInterceptorHandler handler) {
     if (err.response?.statusCode == 403) {
       _handle403Error();
+    } else if (err.response?.statusCode == 401) {
+      _handle401Error();
     } else {
       super.onError(err, handler);
     }
   }
 
+  void _handle401Error() {
+    // call refresh
+  }
+
   void _handle403Error() {
+    // call action
     restartApp();
   }
 }
