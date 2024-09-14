e46c4686602910b70ba339c8ca24cc42434ef91e
msmoradi <mr.ms.moradi@gmail.com>
2024-09-14 22:20:34 +0330

    Fix camera

--- lib/feature/face_detection/presentation/bloc/face_detection_bloc.dart
+++ lib/feature/face_detection/presentation/bloc/face_detection_bloc.dart
@@ -6,6 +6,7 @@ import 'package:banx/core/domain/entities/user_profile_entity.dart';
 import 'package:banx/core/domain/entity_wrapper.dart';
 import 'package:banx/core/domain/repository/media_repository.dart';
 import 'package:banx/core/domain/repository/profile_repository.dart';
+import 'package:banx/feature/face_detection/presentation/bloc/face_detection_state.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
@@ -13,8 +14,6 @@ import 'package:go_router/go_router.dart';
 import 'package:injectable/injectable.dart';
 
 part 'face_detection_event.dart';
-
-part 'face_detection_state.dart';
 
 @injectable
 class FaceDetectionBloc extends Bloc<FaceDetectionEvent, FaceDetectionState> {
@@ -24,33 +23,55 @@ class FaceDetectionBloc extends Bloc<FaceDetectionEvent, FaceDetectionState> {
   FaceDetectionBloc(
     this.mediaRepository,
     this.profileRepository,
-  ) : super(const FaceDetectionSuccess()) {
+  ) : super(const FaceDetectionState()) {
     on<FaceDetectionSubmitted>(_onFaceDetectionSubmitted);
   }
 
   FutureOr<void> _onFaceDetectionSubmitted(
       FaceDetectionSubmitted event, Emitter<FaceDetectionState> emit) async {
-    emit(FaceDetectionInProgress());
     try {
+      emit(state.copyWith(status: FaceDetectionStatus.loading));
       final EntityWrapper<Entity> response = await mediaRepository.uploadVideo(
-          path: event.path, onSendProgress: (a, b) {});
+          path: event.path,
+          onSendProgress: (sent, total) {
+            final progress = ((sent / total) * 100).toInt();
+            emit(state.copyWith(progress: progress));
+          });
 
       response.when(
-          success: (entity) {
-            /*if (entity is UserProfileEntity) {
+        success: (entity) {
+          print('::::::::::::::::::::::::;; success');
+          emit(state.copyWith(status: FaceDetectionStatus.kyc));
+
+          /*if (entity is UserProfileEntity) {
               emit(
                 DeepLinkLanding(deeplink: entity.routingButtonEntity!.deeplink),
               );
             }*/
-            emit(
+          /*emit(
               KycStatus(),
-            );
-          },
-          partialSuccess: (message) => emit(FaceDetectionFailure(message)),
-          networkError: (exception) =>
-              emit(FaceDetectionFailure(exception.toString())));
+            );*/
+        },
+        partialSuccess: (message) => emit(
+          state.copyWith(
+            status: FaceDetectionStatus.failure,
+            errorMessage: message,
+          ),
+        ),
+        networkError: (exception) => emit(
+          state.copyWith(
+            status: FaceDetectionStatus.failure,
+            errorMessage: exception.toString(),
+          ),
+        ),
+      );
     } catch (e) {
-      emit(FaceDetectionFailure(e.toString()));
+      emit(
+        state.copyWith(
+          status: FaceDetectionStatus.failure,
+          errorMessage: e.toString(),
+        ),
+      );
     }
   }
 }
--- lib/feature/face_detection/presentation/view/face_detection_page.dart
+++ lib/feature/face_detection/presentation/view/face_detection_page.dart
@@ -1,17 +1,16 @@
 import 'package:banx/feature/face_detection/presentation/bloc/face_detection_bloc.dart';
-import 'package:banx/feature/face_detection/presentation/view/camera_page.dart';
+import 'package:banx/feature/face_detection/presentation/bloc/face_detection_state.dart';
+import 'package:banx/feature/face_detection/presentation/view/camera_v2_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 class FaceDetectionPage extends StatelessWidget {
-  final Function(String) onDeeplinkLanding;
   final Function() onKycStatus;
   final Function(String) showMessage;
 
   const FaceDetectionPage({
     super.key,
-    required this.onDeeplinkLanding,
     required this.onKycStatus,
     required this.showMessage,
   });
@@ -22,11 +21,18 @@ class FaceDetectionPage extends StatelessWidget {
       create: (context) => GetIt.instance<FaceDetectionBloc>(),
       child: BlocConsumer<FaceDetectionBloc, FaceDetectionState>(
         listener: (context, state) {
-          _handleStateChange(context, state, showMessage);
+          if (state.status == FaceDetectionStatus.failure) {
+            showMessage(state.errorMessage);
+          } else if (state.status == FaceDetectionStatus.kyc) {
+            onKycStatus();
+          }
         },
         builder: (context, state) {
-          return CameraPage(
+          return FlutterCamera(
+            loading: state.status == FaceDetectionStatus.loading,
+            progress: state.progress,
             onVideoRecorded: (path) {
+              print('::::::::::::::::::::::::;; dkdkkd $path');
               context
                   .read<FaceDetectionBloc>()
                   .add(FaceDetectionSubmitted(path));
@@ -35,26 +41,5 @@ class FaceDetectionPage extends StatelessWidget {
         },
       ),
     );
-  }
-
-  void _handleStateChange(
-    BuildContext context,
-    FaceDetectionState state,
-    showMessage,
-  ) {
-    switch (state) {
-      case final FaceDetectionFailure s:
-        {
-          showMessage(s.message);
-        }
-      case final DeepLinkLanding s:
-        {
-          onDeeplinkLanding(s.deeplink);
-        }
-      case final KycStatus s:
-        {
-          onKycStatus();
-        }
-    }
   }
 }
