ecebda97d4036699bf1b73b15acb1f8291181bd6
msmoradi <mr.ms.moradi@gmail.com>
2024-09-14 20:34:00 +0330

    Temp

--- lib/feature/face_detection/presentation/view/camera_page.dart
+++ lib/feature/face_detection/presentation/view/camera_page.dart
@@ -4,6 +4,7 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
+import 'package:permission_handler/permission_handler.dart';
 
 class CameraPage extends StatefulWidget {
   final Function(String) onVideoRecorded;
@@ -21,8 +22,8 @@ class _CameraPageState extends State<CameraPage> {
 
   @override
   void initState() {
-    _initCamera();
     super.initState();
+    _requestPermissions();
   }
 
   @override
@@ -31,21 +32,54 @@ class _CameraPageState extends State<CameraPage> {
     super.dispose();
   }
 
-  _initCamera() async {
+  Future<void> _requestPermissions() async {
+    // Request camera and storage permissions using permission_handler: ^11.3.1
+    PermissionStatus cameraStatus = await Permission.camera.request();
+    PermissionStatus storageStatus = await Permission.storage.request();
+
+    // If permissions are granted, initialize the camera
+    if (cameraStatus.isGranted && storageStatus.isGranted) {
+      _initCamera();
+    } else if (cameraStatus.isPermanentlyDenied || storageStatus.isPermanentlyDenied) {
+      // Handle permanently denied permissions and guide the user to settings
+      await openAppSettings();
+    } else {
+      // Show an error if permissions are denied
+      _showPermissionError();
+    }
+  }
+
+  void _showPermissionError() {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text('Permissions Error'),
+        content: const Text('Camera and storage permissions are required.'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _initCamera() async {
     final cameras = await availableCameras();
     final front = cameras.firstWhere(
-        (camera) => camera.lensDirection == CameraLensDirection.front);
+            (camera) => camera.lensDirection == CameraLensDirection.front);
     _cameraController = CameraController(front, ResolutionPreset.max);
     await _cameraController.initialize();
     setState(() => _isLoading = false);
   }
 
-  _recordVideo(BuildContext context) async {
+  Future<void> _recordVideo(BuildContext context) async {
     if (_isRecording) {
       final file = await _cameraController.stopVideoRecording();
-      final File tempFile = File(file.path); // Convert XFile to File
+      final File tempFile = File(file.path);
 
-      final directory = await getTemporaryDirectory();
+      final directory = await getApplicationCacheDirectory();
       final newVideoPath = path.join(
           directory.path, '${DateTime.now().millisecondsSinceEpoch}.mp4');
 
