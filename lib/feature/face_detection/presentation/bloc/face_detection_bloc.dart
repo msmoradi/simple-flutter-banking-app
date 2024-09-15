d0e3ae349577cfa506fc59ea083b4ba00f784d66
msmoradi <mr.ms.moradi@gmail.com>
2024-09-15 14:41:23 +0330

    Temp send file

--- lib/feature/face_detection/presentation/bloc/face_detection_bloc.dart
+++ lib/feature/face_detection/presentation/bloc/face_detection_bloc.dart
@@ -1,17 +1,16 @@
 import 'dart:async';
-
-import 'package:banx/core/data/model/user_profile_response_dto.dart';
 import 'package:banx/core/domain/entities/entity.dart';
-import 'package:banx/core/domain/entities/user_profile_entity.dart';
 import 'package:banx/core/domain/entity_wrapper.dart';
 import 'package:banx/core/domain/repository/media_repository.dart';
 import 'package:banx/core/domain/repository/profile_repository.dart';
 import 'package:banx/feature/face_detection/presentation/bloc/face_detection_state.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter/services.dart';
 import 'package:injectable/injectable.dart';
+import 'dart:io';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:path_provider/path_provider.dart';
 
 part 'face_detection_event.dart';
 
@@ -27,12 +26,33 @@ class FaceDetectionBloc extends Bloc<FaceDetectionEvent, FaceDetectionState> {
     on<FaceDetectionSubmitted>(_onFaceDetectionSubmitted);
   }
 
+  // Function to get the image path from assets
+  Future<String> getImagePathFromAssets(String assetName) async {
+    // Load image data from assets
+    ByteData byteData = await rootBundle.load('assets/images/$assetName');
+
+    // Get the temporary directory
+    Directory tempDir = await getTemporaryDirectory();
+
+    // Create a temporary file
+    File tempFile = File('${tempDir.path}/$assetName');
+
+    // Write the image data to the temporary file
+    await tempFile.writeAsBytes(byteData.buffer.asUint8List());
+
+    // Return the path of the temporary file
+    return tempFile.path;
+  }
+
   FutureOr<void> _onFaceDetectionSubmitted(
       FaceDetectionSubmitted event, Emitter<FaceDetectionState> emit) async {
     try {
       emit(state.copyWith(status: FaceDetectionStatus.loading));
+
+      String imagePath = await getImagePathFromAssets('x1.png');
+
       final EntityWrapper<Entity> response = await mediaRepository.uploadVideo(
-          path: event.path,
+          path: imagePath,
           onSendProgress: (sent, total) {
             final progress = ((sent / total) * 100).toInt();
             emit(state.copyWith(progress: progress));
