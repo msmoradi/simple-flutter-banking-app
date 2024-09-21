3dfe1bce3b0e78ba6ac8330a7db8a71ab222e767
msmoradi <mr.ms.moradi@gmail.com>
2024-09-21 13:02:18 +0330

    Change repository

--- lib/core/domain/repository/passkey_repository.dart
+++ lib/core/domain/repository/passkey_repository.dart
@@ -1,23 +1,15 @@
-
 import 'package:banx/core/data/model/request/webauthn/passkey_assertion_options_request_dto.dart';
-import 'package:banx/core/data/model/request/webauthn/passkey_assertion_result_request_dto.dart';
 import 'package:banx/core/data/model/request/webauthn/passkey_attestation_options_request_dto.dart';
-import 'package:banx/core/data/model/request/webauthn/passkey_attestation_request_dto.dart';
-import 'package:banx/core/domain/entities/passkey_assertion_options.dart';
 import 'package:banx/core/domain/entities/passkey_assertion_result.dart';
 import 'package:banx/core/domain/entities/passkey_attestation.dart';
-import 'package:banx/core/domain/entities/passkey_attestation_options.dart';
 
 abstract class PasskeyRepository {
-  Future<PasskeyAttestation> sendPasskeyAttestation(
-      PasskeyAttestationRequestDTO request);
 
-  Future<PasskeyAttestationOptions> getPasskeyAttestationOptions(
-      PasskeyAttestationOptionsRequestDTO request);
-
-  Future<PasskeyAssertionResult> sendPasskeyAssertionResult(
-      PasskeyAssertionResultRequestDTO request);
-
-  Future<PasskeyAssertionOptions> getPasskeyAssertionOptions(
+  @override
+  Future<PasskeyAssertionResult> register(
       PasskeyAssertionOptionsRequestDTO request);
+
+  @override
+  Future<PasskeyAttestation> signIn(
+      PasskeyAttestationOptionsRequestDTO request);
 }
