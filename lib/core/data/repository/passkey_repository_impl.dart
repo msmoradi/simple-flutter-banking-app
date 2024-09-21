bd7f5d52bb0528d54656b39ca11bcf6a87be6b1a
msmoradi <mr.ms.moradi@gmail.com>
2024-09-21 13:02:18 +0330

    Change repository

--- lib/core/data/repository/passkey_repository_impl.dart
+++ lib/core/data/repository/passkey_repository_impl.dart
@@ -2,50 +2,30 @@ import 'package:banx/core/data/datasource/remote/passkey_remote_data_source.dart
 import 'package:banx/core/data/model/request/webauthn/passkey_assertion_options_request_dto.dart';
 import 'package:banx/core/data/model/request/webauthn/passkey_assertion_result_request_dto.dart';
 import 'package:banx/core/data/model/request/webauthn/passkey_attestation_options_request_dto.dart';
-import 'package:banx/core/data/model/request/webauthn/passkey_attestation_request_dto.dart';
-import 'package:banx/core/domain/entities/passkey_assertion_options.dart';
 import 'package:banx/core/domain/entities/passkey_assertion_result.dart';
 import 'package:banx/core/domain/entities/passkey_attestation.dart';
-import 'package:banx/core/domain/entities/passkey_attestation_options.dart';
 import 'package:banx/core/domain/repository/passkey_repository.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
 
 @LazySingleton(as: PasskeyRepository)
 class PasskeyRepositoryImpl implements PasskeyRepository {
   final PasskeyRemoteDataSource remoteDataSource;
-  final FlutterSecureStorage secureStorage;
 
   PasskeyRepositoryImpl({
     required this.remoteDataSource,
-    required this.secureStorage,
   });
 
   @override
-  Future<PasskeyAssertionOptions> getPasskeyAssertionOptions(
+  Future<PasskeyAssertionResult> register(
       PasskeyAssertionOptionsRequestDTO request) {
-    // TODO: implement getPasskeyAssertionOptions
+    // TODO: implement signIn
     throw UnimplementedError();
   }
 
   @override
-  Future<PasskeyAttestationOptions> getPasskeyAttestationOptions(
+  Future<PasskeyAttestation> signIn(
       PasskeyAttestationOptionsRequestDTO request) {
-    // TODO: implement getPasskeyAttestationOptions
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<PasskeyAssertionResult> sendPasskeyAssertionResult(
-      PasskeyAssertionResultRequestDTO request) {
-    // TODO: implement sendPasskeyAssertionResult
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<PasskeyAttestation> sendPasskeyAttestation(
-      PasskeyAttestationRequestDTO request) {
-    // TODO: implement sendPasskeyAttestation
+    // TODO: implement signIn
     throw UnimplementedError();
   }
 }
