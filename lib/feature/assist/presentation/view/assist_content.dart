6dece2a2455c852ff15d90e1ee343e67a81e9929
msmoradi <mr.ms.moradi@gmail.com>
2024-09-29 19:49:44 +0330

    Add nfc

--- lib/feature/assist/presentation/view/assist_content.dart
+++ lib/feature/assist/presentation/view/assist_content.dart
@@ -1,6 +1,15 @@
+import 'dart:io';
+
+import 'package:banx/feature/assist/presentation/ndef_record/raw_record_setting.dart';
+import 'package:banx/feature/assist/presentation/ndef_record/text_record_setting.dart';
+import 'package:banx/feature/assist/presentation/ndef_record/uri_record_setting.dart';
+import 'package:banx/feature/assist/presentation/view/nfc_apdu_screen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:nfc_manager/nfc_manager.dart';
-import 'dart:typed_data';
+import 'package:flutter/services.dart';
+import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
+import 'package:ndef/ndef.dart' as ndef;
+import 'package:ndef/utilities.dart';
 
 class AssistContent extends StatefulWidget {
   const AssistContent({super.key});
@@ -9,118 +18,154 @@ class AssistContent extends StatefulWidget {
   State<AssistContent> createState() => _AssistContentState();
 }
 
-class _AssistContentState extends State<AssistContent> {
-  ValueNotifier<dynamic> result = ValueNotifier(null);
+class _AssistContentState extends State<AssistContent>
+    with SingleTickerProviderStateMixin {
+  String _platformVersion = '';
+  NFCAvailability _availability = NFCAvailability.not_supported;
+  NFCTag? _tag;
+  String? _result, _writeResult, _mifareResult;
+  late TabController _tabController;
+  List<ndef.NDEFRecord>? _records;
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (!kIsWeb)
+      _platformVersion =
+          '${Platform.operatingSystem} ${Platform.operatingSystemVersion}';
+    else
+      _platformVersion = 'Web';
+    initPlatformState();
+    _tabController = new TabController(length: 2, vsync: this);
+    _records = [];
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    NFCAvailability availability;
+    try {
+      availability = await FlutterNfcKit.nfcAvailability;
+    } on PlatformException {
+      availability = NFCAvailability.not_supported;
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      // _platformVersion = platformVersion;
+      _availability = availability;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('NfcManager Plugin Example')),
-      body: SafeArea(
-        child: FutureBuilder<bool>(
-          future: NfcManager.instance.isAvailable(),
-          builder: (context, ss) => ss.data != true
-              ? Center(child: Text('NfcManager.isAvailable(): ${ss.data}'))
-              : Flex(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  direction: Axis.vertical,
-                  children: [
-                    Flexible(
-                      flex: 2,
-                      child: Container(
-                        margin: EdgeInsets.all(4),
-                        constraints: BoxConstraints.expand(),
-                        decoration: BoxDecoration(border: Border.all()),
-                        child: SingleChildScrollView(
-                          child: ValueListenableBuilder<dynamic>(
-                            valueListenable: result,
-                            builder: (context, value, _) =>
-                                Text('${value ?? ''}'),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Flexible(
-                      flex: 3,
-                      child: GridView.count(
-                        padding: EdgeInsets.all(4),
-                        crossAxisCount: 2,
-                        childAspectRatio: 4,
-                        crossAxisSpacing: 4,
-                        mainAxisSpacing: 4,
-                        children: [
-                          ElevatedButton(
-                              child: Text('Tag Read'), onPressed: _tagRead),
-                          ElevatedButton(
-                              child: Text('Ndef Write'), onPressed: _ndefWrite),
-                          ElevatedButton(
-                              child: Text('Ndef Write Lock'),
-                              onPressed: _ndefWriteLock),
-                        ],
-                      ),
-                    ),
-                  ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            title: const Text('NFC Flutter Kit Example App'),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(text: 'Read'),
+                Tab(text: 'Write'),
+              ],
+              controller: _tabController,
+            )),
+        body: new TabBarView(controller: _tabController, children: <Widget>[
+          Scrollbar(
+              child: SingleChildScrollView(
+                  child: Center(
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                const SizedBox(height: 20),
+                Text('Running on: $_platformVersion\nNFC: $_availability'),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      NFCTag tag = await FlutterNfcKit.poll();
+                      setState(() {
+                        _tag = tag;
+                      });
+                      await FlutterNfcKit.setIosAlertMessage(
+                          "Working on it...");
+                      _mifareResult = null;
+                      if (tag.standard == "ISO 14443-4 (Type B)") {
+                        String result1 =
+                            await FlutterNfcKit.transceive("00B0950000");
+                        String result2 = await FlutterNfcKit.transceive(
+                            "00A4040009A00000000386980701");
+                        setState(() {
+                          _result = '1: $result1\n2: $result2\n';
+                        });
+                      } else if (tag.type == NFCTagType.iso18092) {
+                        String result1 =
+                            await FlutterNfcKit.transceive("060080080100");
+                        setState(() {
+                          _result = '1: $result1\n';
+                        });
+                      } else if (tag.ndefAvailable ?? false) {
+                        var ndefRecords = await FlutterNfcKit.readNDEFRecords();
+                        var ndefString = '';
+                        for (int i = 0; i < ndefRecords.length; i++) {
+                          ndefString += '${i + 1}: ${ndefRecords[i]}\n';
+                        }
+                        setState(() {
+                          _result = ndefString;
+                        });
+                      } else if (tag.type == NFCTagType.webusb) {
+                        var r = await FlutterNfcKit.transceive(
+                            "00A4040006D27600012401");
+                        print(r);
+                      }
+                    } catch (e) {
+                      setState(() {
+                        _result = 'error: $e';
+                      });
+                    }
+
+                    // Pretend that we are working
+                    if (!kIsWeb) sleep(new Duration(seconds: 1));
+                    await FlutterNfcKit.finish(iosAlertMessage: "Finished!");
+                  },
+                  child: Text('Start polling'),
                 ),
-        ),
+                const SizedBox(height: 10),
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: _tag != null
+                        ? Text(
+                            'ID: ${_tag!.id}\nStandard: ${_tag!.standard}\nType: ${_tag!.type}\nATQA: ${_tag!.atqa}\nSAK: ${_tag!.sak}\nHistorical Bytes: ${_tag!.historicalBytes}\nProtocol Info: ${_tag!.protocolInfo}\nApplication Data: ${_tag!.applicationData}\nHigher Layer Response: ${_tag!.hiLayerResponse}\nManufacturer: ${_tag!.manufacturer}\nSystem Code: ${_tag!.systemCode}\nDSF ID: ${_tag!.dsfId}\nNDEF Available: ${_tag!.ndefAvailable}\nNDEF Type: ${_tag!.ndefType}\nNDEF Writable: ${_tag!.ndefWritable}\nNDEF Can Make Read Only: ${_tag!.ndefCanMakeReadOnly}\nNDEF Capacity: ${_tag!.ndefCapacity}\nMifare Info:${_tag!.mifareInfo} Transceive Result:\n$_result\n\nBlock Message:\n$_mifareResult')
+                        : const Text('No tag polled yet.')),
+              ])))),
+          NfcApduScreen()
+        ]),
       ),
     );
   }
 
-  void _tagRead() {
-    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
-      result.value = tag.data;
-      NfcManager.instance.stopSession();
-    });
-  }
+  Future<void> sendApduCommand() async {
+    // Example APDU command
+    print('sendApduCommand');
 
-  void _ndefWrite() {
-    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
-      var ndef = Ndef.from(tag);
-      if (ndef == null || !ndef.isWritable) {
-        result.value = 'Tag is not ndef writable';
-        NfcManager.instance.stopSession(errorMessage: result.value);
-        return;
-      }
+    const String apduCommand = '00 B2 01 0C 00';
 
-      NdefMessage message = NdefMessage([
-        NdefRecord.createText('Hello World!'),
-        NdefRecord.createUri(Uri.parse('https://flutter.dev')),
-        NdefRecord.createMime(
-            'text/plain', Uint8List.fromList('Hello'.codeUnits)),
-        NdefRecord.createExternal(
-            'com.example', 'mytype', Uint8List.fromList('mydata'.codeUnits)),
-      ]);
-
-      try {
-        await ndef.write(message);
-        result.value = 'Success to "Ndef Write"';
-        NfcManager.instance.stopSession();
-      } catch (e) {
-        result.value = e;
-        NfcManager.instance.stopSession(errorMessage: result.value.toString());
-        return;
-      }
-    });
-  }
-
-  void _ndefWriteLock() {
-    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
-      var ndef = Ndef.from(tag);
-      if (ndef == null) {
-        result.value = 'Tag is not ndef';
-        NfcManager.instance.stopSession(errorMessage: result.value.toString());
-        return;
-      }
-
-      try {
-        await ndef.writeLock();
-        result.value = 'Success to "Ndef Write Lock"';
-        NfcManager.instance.stopSession();
-      } catch (e) {
-        result.value = e;
-        NfcManager.instance.stopSession(errorMessage: result.value.toString());
-        return;
-      }
-    });
+    try {
+      // Send the APDU command
+      var response = await FlutterNfcKit.transceive(apduCommand);
+      print('APDU response: $response');
+    } catch (e) {
+      // Handle any communication errors
+      print('APDU communication error: $e');
+    }
   }
 }
