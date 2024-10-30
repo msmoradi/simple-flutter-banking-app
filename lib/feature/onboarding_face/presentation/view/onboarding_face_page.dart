e063de0b7a71c33e976578fd04bca7aebd763cd0
msmoradi <mr.ms.moradi@gmail.com>
2024-10-30 20:42:48 +0330

    Fix OnboardingFace Page

--- lib/feature/onboarding_face/presentation/view/onboarding_face_page.dart
+++ lib/feature/onboarding_face/presentation/view/onboarding_face_page.dart
@@ -27,70 +27,73 @@ class OnboardingFacePage extends StatelessWidget {
           "تشخیص چهره",
         ),
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            children: [
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          style:
-                          Theme.of(context).textTheme.bodyMedium?.copyWith(
-                            color: Theme.of(context)
-                                .colorScheme
-                                .onSurfaceVariant,
+      body: Stack(alignment: Alignment.bottomRight, children: [
+        Image.asset(
+          "assets/images/liveness_detection.png",
+          fit: BoxFit.fitWidth,
+          width: double.infinity,
+          alignment: Alignment.center,
+        ),
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium
+                                ?.copyWith(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onSurfaceVariant,
+                                ),
+                            "جهت احراز هویت دقیق به نکات زیر دقت کنید",
                           ),
-                          "جهت احراز هویت دقیق به نکات زیر دقت کنید",
                         ),
-                      ),
-                      const SizedBox(height: 80),
-                      const InfoTextRowWidget(
-                        iconAsset: 'assets/icons/faceid.svg',
-                        title: 'حرکت چهره',
-                        subtitle:
-                        'سر خود را مقابل دوربین به چپ و راست حرکت دهید تا پیام تأیید روی صفحه نمایش داده شود',
-                      ),
-                      const SizedBox(height: 30),
-                      const InfoTextRowWidget(
-                        iconAsset: 'assets/icons/bulb-on.svg',
-                        title: 'نور محیط و پس‌زمینه',
-                        subtitle:
-                        'در زمان ثبت ویدئو، لطفا نور محیط، کافی و پس‌زمینه شما سفید باشد',
-                      ),
-                      const SizedBox(height: 50),
-                      Image.asset(
-                        "assets/images/onboarding_face.png",
-                        fit: BoxFit.fitWidth,
-                        width: double.infinity,
-                        alignment: Alignment.center,
-                      ),
-                      const SizedBox(height: 50),
-                    ],
+                        const SizedBox(height: 80),
+                        const InfoTextRowWidget(
+                          iconAsset: 'assets/icons/faceid.svg',
+                          title: 'حرکت چهره',
+                          subtitle:
+                              'سر خود را مقابل دوربین به چپ و راست حرکت دهید تا پیام تأیید روی صفحه نمایش داده شود',
+                        ),
+                        const SizedBox(height: 30),
+                        const InfoTextRowWidget(
+                          iconAsset: 'assets/icons/bulb-on.svg',
+                          title: 'نور محیط و پس‌زمینه',
+                          subtitle:
+                              'در زمان ثبت ویدئو، لطفا نور محیط، کافی و پس‌زمینه شما سفید باشد',
+                        ),
+                        const SizedBox(height: 50),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              PrimaryFillButton(
-                iconAlignment: IconAlignment.start,
-                icon: SvgPicture.asset(
-                  'assets/icons/thumb-up.svg',
-                  colorFilter: ColorFilter.mode(
-                    Theme.of(context).colorScheme.onPrimary,
-                    BlendMode.srcIn,
+                PrimaryFillButton(
+                  iconAlignment: IconAlignment.start,
+                  icon: SvgPicture.asset(
+                    'assets/icons/thumb-up.svg',
+                    colorFilter: ColorFilter.mode(
+                      Theme.of(context).colorScheme.onPrimary,
+                      BlendMode.srcIn,
+                    ),
                   ),
+                  label: 'متوجه شدم',
+                  onPressed: onNext,
                 ),
-                label: 'متوجه شدم',
-                onPressed: onNext,
-              ),
-              const SizedBox(height: 16)
-            ],
+                const SizedBox(height: 16)
+              ],
+            ),
           ),
         ),
-      ),
+      ]),
     );
   }
 }
